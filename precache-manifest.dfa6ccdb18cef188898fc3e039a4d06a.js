17782345281751c43094b7830bdbcc42f585c566
anufreichik <marta149@hotmail.com>
2020-09-28 17:51:20 -0700

    Updates

---
+++ precache-manifest.dfa6ccdb18cef188898fc3e039a4d06a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724059353923cfd23a9d66b17defc117",
+    "url": "/snake-apple-game/index.html"
+  },
+  {
+    "revision": "8b50554ec372fe80e360",
+    "url": "/snake-apple-game/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0d4d9c6484ffe2d468ce",
+    "url": "/snake-apple-game/static/css/main.09e1bb96.chunk.css"
+  },
+  {
+    "revision": "8b50554ec372fe80e360",
+    "url": "/snake-apple-game/static/js/2.32ce76d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-apple-game/static/js/2.32ce76d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4d9c6484ffe2d468ce",
+    "url": "/snake-apple-game/static/js/main.ee1f1b70.chunk.js"
+  },
+  {
+    "revision": "00f01e039f882d8e5ce1",
+    "url": "/snake-apple-game/static/js/runtime-main.5e891e2b.js"
+  }
+]);
